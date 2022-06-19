9ac7a782fdf6423149e8c75b5213283cbd7e145c
nojoven <work.cedricjoseph@gmail.com>
2022-06-19 17:48:58 +0200

    feat: add basic profile model

--- waw/core/models.py
+++ waw/core/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
 class Profile(models.Model):
-   user_name = None
-   user_id = None
-   bio = None
-   picture = models.ImageField('')
-   location = None 
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   id_user = models.IntegerField()
+   bio = models.TextField(blank=True)
+   picture = models.ImageField(upload_to='profile_pictures', default='blank_profile.jpg')
+   location = models.CharField(max_length=100, blank=True) 
+
+   def __str__(self):
+      return self.user.username
