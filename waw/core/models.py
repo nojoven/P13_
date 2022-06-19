001deb900c043a39d81fc9b4c8008dc72f38f190
nojoven <work.cedricjoseph@gmail.com>
2022-06-19 17:27:50 +0200

    feat: add media folder.

--- waw/core/models.py
+++ waw/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+   user_name = None
+   user_id = None
+   bio = None
+   picture = models.ImageField('')
+   location = None 
